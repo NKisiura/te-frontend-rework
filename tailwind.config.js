2cda4703b996a55b1fc147449dd801f5dde32788
leonidsubin <leonardoshubin@gmail.com>
2023-06-28 11:27:56 +0300

    tailwind implemented

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: 'class',
+}
+
+// !!! for color variables !!!
+// theme: {
+//   colors: {
+//     primary: 'rgb(var(--color-primary) / <alpha-value>)',
+//     secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+//   }
+// }
