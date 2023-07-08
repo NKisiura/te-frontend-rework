a4fbf304de4b5a084885740507b19d1d97ffaa9a
Nikita Kisiura <nikitar156@gmail.com>
2023-07-08 13:29:14 +0300

    session-params-state moved to dashboard module

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -5,6 +5,11 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { StoreModule } from '@ngrx/store';
+import { sessionParamsFeatureKey } from './state/session-params/session-params-feature-key';
+import { sessionParamsReducer } from './state/session-params/session-params.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { SessionParamsEffect } from './state/session-params/session-params.effect';
 
 @NgModule({
   declarations: [
@@ -13,6 +18,11 @@ import { MenuComponent } from './components/menu/menu.component';
     FooterComponent,
     MenuComponent,
   ],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    StoreModule.forFeature(sessionParamsFeatureKey, sessionParamsReducer),
+    EffectsModule.forFeature([SessionParamsEffect]),
+  ],
 })
 export class DashboardModule {}
