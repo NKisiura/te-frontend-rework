bf6fb17045aad89991313a1dd90518d687f2d194
Nikita Kisiura <nikitar156@gmail.com>
2023-07-14 11:29:27 +0300

    routing loader created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public routingLoading = false;
+
+  constructor(public router: Router) {}
+
+  ngOnInit(): void {
+    this.initListeners();
+  }
+
+  private initListeners(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.routingLoading = true;
+      }
+      if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      )
+        this.routingLoading = false;
+    });
+  }
+}
