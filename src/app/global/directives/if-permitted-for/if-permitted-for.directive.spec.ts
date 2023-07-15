112673cefcb1899fd8c4abad524c4a684e5a8867
Nikita Kisiura <nikitar156@gmail.com>
2023-07-15 14:27:02 +0300

    te-icon changed to standalone component

--- src/app/global/directives/if-permitted-for/if-permitted-for.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IfPermittedForDirective } from './if-permitted-for.directive';
-
-describe('IfPermittedForDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IfPermittedForDirective();
-    expect(directive).toBeTruthy();
-  });
-});
