4851331360a184fb0ee7c819c31cd469e18b4dd3
Nikita Kisiura <nikitar156@gmail.com>
2023-07-20 17:59:35 +0300

    created icons-preview story

---
+++ src/app/global/components/te-icon/stories/te-icon.stories.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { TeIconStoriesPreviewComponent } from '@global/components/te-icon/stories/te-icon-stories-preview.component';
+
+const meta: Meta = {
+  title: 'Icons/Preview',
+  component: TeIconStoriesPreviewComponent,
+};
+
+export default meta;
+type Story = StoryObj<TeIconStoriesPreviewComponent>;
+
+export const Preview: Story = {};
